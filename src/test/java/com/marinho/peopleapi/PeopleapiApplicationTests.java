7484d6df7d6bd04f1f6733df53b42d62610276da
marinholucasit <marinholucasit@gmail.com>
2021-08-07 16:34:56 -0300

    init project

---
+++ src/test/java/com/marinho/peopleapi/PeopleapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marinho.peopleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
